521b8221dff8723240d936a8da61079168df0db2
yeonkyung <younkyong94@gmail.com>
2022-12-07 14:29:37 +0900

    2022.12.07 - AOP

---
+++ src/main/java/hello/advanced/proxy/config/AopConfig.java
@@ -0,0 +1,16 @@
+package hello.advanced.proxy.config;
+
+import hello.advanced.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({AppV1Config.class, AppV2Config.class})
+public class AopConfig {
+
+    @Bean
+    public LogTraceAspect logTraceAspect(LogTrace logTrace) {
+        return new LogTraceAspect(logTrace);
+    }
+}
